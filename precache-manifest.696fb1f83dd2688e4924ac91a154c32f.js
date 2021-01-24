ee278da34d52365b2d1ea9b433de004d5be2fbd8
roderick-fuentes-ramos <ramos.roderick13@gmail.com>
2021-01-25 02:41:44 +0800

    Updates

---
+++ precache-manifest.696fb1f83dd2688e4924ac91a154c32f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bb1a6531234964369c3",
+    "url": "/static/css/main.962a1e18.chunk.css"
+  },
+  {
+    "revision": "1bb1a6531234964369c3",
+    "url": "/static/js/main.1bb1a653.chunk.js"
+  },
+  {
+    "revision": "8256f6d2eb5559535093",
+    "url": "/static/js/1.8256f6d2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b7523ddfae3a64f66b7d128934182b1",
+    "url": "/index.html"
+  }
+];
