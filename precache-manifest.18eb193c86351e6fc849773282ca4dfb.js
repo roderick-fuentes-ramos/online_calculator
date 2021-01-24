36a7068860dc1a962d63257c5f02661c49e8cd7a
roderick-fuentes-ramos <ramos.roderick13@gmail.com>
2021-01-25 03:14:07 +0800

    Updates

---
+++ precache-manifest.18eb193c86351e6fc849773282ca4dfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d3298e8297a6b1ba6c1",
+    "url": "/roderick-fuentes-ramos/online_calculator/static/css/main.b3a8efd0.chunk.css"
+  },
+  {
+    "revision": "1d3298e8297a6b1ba6c1",
+    "url": "/roderick-fuentes-ramos/online_calculator/static/js/main.3642ada4.chunk.js"
+  },
+  {
+    "revision": "e52f8bc65a0b3924f465",
+    "url": "/roderick-fuentes-ramos/online_calculator/static/js/runtime~main.aff04352.js"
+  },
+  {
+    "revision": "1a0d3d27faed8d027ab9",
+    "url": "/roderick-fuentes-ramos/online_calculator/static/js/2.06709f86.chunk.js"
+  },
+  {
+    "revision": "63673bb34a0637e1bf21244259195cf1",
+    "url": "/roderick-fuentes-ramos/online_calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27d21c2633eac760efbecc88b7dec8a1.js"
+  "/roderick-fuentes-ramos/online_calculator/precache-manifest.18eb193c86351e6fc849773282ca4dfb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/roderick-fuentes-ramos/online_calculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
